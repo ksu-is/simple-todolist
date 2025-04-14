d5b8530112a885e1d6f89d207c64e434184f4e55
myshies <strinh1@students.kennesaw.edu>
2025-04-13 22:01:33 -0400

    finished the initial layout of the todo list and the introduction of what the user would see

--- todoapp/todoapp.py
+++ todoapp/todoapp.py
@@ -1,2 +1,12 @@
-print("hello world!")
+tasks = []
+
+def show_menu():
+    """Display the menu options"""
+    print("\n===== TO-DO LIST =====")
+    print("1. View all tasks")
+    print("2. Add a task")
+    print("3. Mark task as complete")
+    print("4. Delete a task")
+    print("5. Exit")
+    print("=====================")
       
